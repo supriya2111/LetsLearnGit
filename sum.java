b50881052f8ba61c01943f8cd25f0ec8f37ebf2f
Supriya Sutar <supriyasutar543@gmail.com>
2022-07-20 19:13:48 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.*;
+
+public class sum{
+
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+
+        int a,b,c;
+        System.out.rintln("Enter any two numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a+b;
+        System.out.println("Sum is:"+c);
+    }
+
+}
